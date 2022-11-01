3c4947700ff6de0bb9a3d853fe817e6051225fbd
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2022-11-01 10:26:30 -0700

    Add files via upload

---
+++ BD/plugins/OnlineFriendCount.plugin.js
@@ -0,0 +1,454 @@
+/**
+ * @name OnlineFriendCount
+ * @author Zerthox
+ * @version 2.2.1
+ * @description Add the old online friend count back to guild list. Because nostalgia.
+ * @authorLink https://github.com/Zerthox
+ * @website https://github.com/Zerthox/BetterDiscord-Plugins
+ * @source https://github.com/Zerthox/BetterDiscord-Plugins/tree/master/src/OnlineFriendCount
+**/
+
+/*@cc_on @if (@_jscript)
+var pluginName = WScript.ScriptName.split(".")[0];
+var shell = WScript.CreateObject("WScript.Shell");
+shell.Popup(
+    "Do NOT run scripts from the internet with the Windows Script Host!\nMove this file to your BetterDiscord plugins folder.",
+    0,
+    pluginName + ": Warning!",
+    0x1030
+);
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var pluginsPath = shell.expandEnvironmentStrings("%appdata%\\BetterDiscord\\plugins");
+if (!fso.FolderExists(pluginsPath)) {
+    var popup = shell.Popup(
+        "Unable to find BetterDiscord on your computer.\nOpen the download page of BetterDiscord?",
+        0,
+        pluginName + ": BetterDiscord not found",
+        0x34
+    );
+    if (popup === 6) {
+        shell.Exec("explorer \"https://betterdiscord.app\"");
+    }
+} else if (WScript.ScriptFullName === pluginsPath + "\\" + WScript.ScriptName) {
+    shell.Popup(
+        "This plugin is already in the correct folder.\nNavigate to the \"Plugins\" settings tab in Discord and enable it there.",
+        0,
+        pluginName,
+        0x40
+    );
+} else {
+    var popup = shell.Popup(
+        "Open the BetterDiscord plugins folder?",
+        0,
+        pluginName,
+        0x34
+    );
+    if (popup === 6) {
+        shell.Exec("explorer " + pluginsPath);
+    }
+}
+WScript.Quit();
+@else @*/
+
+'use strict';
+
+const createData = (id) => ({
+    load: (key) => BdApi.Data.load(id, key) ?? null,
+    save: (key, value) => BdApi.Data.save(id, key, value),
+    delete: (key) => BdApi.Data.delete(id, key)
+});
+
+const createLazy = () => {
+    let controller = new AbortController();
+    return {
+        waitFor: (filter, { resolve = true, entries = false }) => BdApi.Webpack.waitForModule(filter, {
+            signal: controller.signal,
+            defaultExport: resolve,
+            searchExports: entries
+        }),
+        abort: () => {
+            controller.abort();
+            controller = new AbortController();
+        }
+    };
+};
+
+const createLogger = (name, color, version) => {
+    const print = (output, ...data) => output(`%c[${name}] %c${version ? `(v${version})` : ""}`, `color: ${color}; font-weight: 700;`, "color: #666; font-size: .8em;", ...data);
+    return {
+        print,
+        log: (...data) => print(console.log, ...data),
+        warn: (...data) => print(console.warn, ...data),
+        error: (...data) => print(console.error, ...data)
+    };
+};
+
+const createPatcher = (id, Logger) => {
+    const forward = (patcher, type, object, method, callback, options) => {
+        const original = object?.[method];
+        if (!(original instanceof Function)) {
+            throw TypeError(`patch target ${original} is not a function`);
+        }
+        const cancel = patcher[type](id, object, method, options.once ? (...args) => {
+            const result = callback(cancel, original, ...args);
+            cancel();
+            return result;
+        } : (...args) => callback(cancel, original, ...args));
+        if (!options.silent) {
+            Logger.log(`Patched ${options.name ?? String(method)}`);
+        }
+        return cancel;
+    };
+    return {
+        instead: (object, method, callback, options = {}) => forward(BdApi.Patcher, "instead", object, method, (cancel, original, context, args) => callback({ cancel, original, context, args }), options),
+        before: (object, method, callback, options = {}) => forward(BdApi.Patcher, "before", object, method, (cancel, original, context, args) => callback({ cancel, original, context, args }), options),
+        after: (object, method, callback, options = {}) => forward(BdApi.Patcher, "after", object, method, (cancel, original, context, args, result) => callback({ cancel, original, context, args, result }), options),
+        unpatchAll: () => {
+            if (BdApi.Patcher.getPatchesByCaller(id).length > 0) {
+                BdApi.Patcher.unpatchAll(id);
+                Logger.log("Unpatched all");
+            }
+        }
+    };
+};
+
+const byName$1 = (name) => {
+    return (target) => (target?.displayName ?? target?.constructor?.displayName) === name;
+};
+const byProps$1 = (...props) => {
+    return (target) => target instanceof Object && props.every((prop) => prop in target);
+};
+const byProtos$1 = (...protos) => {
+    return (target) => target instanceof Object && target.prototype instanceof Object && protos.every((proto) => proto in target.prototype);
+};
+const bySource$1 = (...fragments) => {
+    return (target) => {
+        if (target instanceof Function) {
+            const source = target.toString();
+            const renderSource = target.prototype?.render?.toString();
+            return fragments.every((fragment) => (typeof fragment === "string" ? (source.includes(fragment) || renderSource?.includes(fragment)) : (fragment(source) || renderSource && fragment(renderSource))));
+        }
+        else if (target instanceof Object && "$$typeof" in target) {
+            const source = (target.render ?? target.type)?.toString();
+            return source && fragments.every((fragment) => typeof fragment === "string" ? source.includes(fragment) : fragment(source));
+        }
+        else {
+            return false;
+        }
+    };
+};
+
+const find = (filter, { resolve = true, entries = false } = {}) => BdApi.Webpack.getModule(filter, {
+    defaultExport: resolve,
+    searchExports: entries
+});
+const byName = (name, options) => find(byName$1(name), options);
+const byProps = (props, options) => find(byProps$1(...props), options);
+const byProtos = (protos, options) => find(byProtos$1(...protos), options);
+const bySource = (contents, options) => find(bySource$1(...contents), options);
+const demangle = (mapping, required, resolve = true) => {
+    const req = required ?? Object.keys(mapping);
+    const found = find((exports) => (exports instanceof Object
+        && exports !== window
+        && req.every((req) => {
+            const filter = mapping[req];
+            return typeof filter === "string"
+                ? filter in exports
+                : Object.values(exports).some((value) => filter(value));
+        })));
+    return resolve ? Object.fromEntries(Object.entries(mapping).map(([key, filter]) => [
+        key,
+        typeof filter === "string" ? found?.[filter] : Object.values(found ?? {}).find((value) => filter(value))
+    ])) : found;
+};
+
+const OldFlux = /* @__PURE__ */ byProps(["Store"]);
+const Flux = {
+    default: OldFlux,
+    Store: OldFlux?.Store,
+    Dispatcher: /* @__PURE__ */ byProtos(["dispatch", "unsubscribe"], { entries: true }),
+    useStateFromStores: /* @__PURE__ */ bySource(["useStateFromStores"], { entries: true })
+};
+
+const { React } = BdApi;
+const { ReactDOM } = BdApi;
+const classNames = /* @__PURE__ */ find((exports) => exports instanceof Object && exports.default === exports && Object.keys(exports).length === 1);
+
+const PresenceStore = /* @__PURE__ */ byName("PresenceStore");
+const RelationshipStore = /* @__PURE__ */ byName("RelationshipStore");
+
+class Settings extends Flux.Store {
+    constructor(Data, defaults) {
+        super(new Flux.Dispatcher(), {
+            update: () => {
+                for (const listener of this.listeners) {
+                    listener(this.current);
+                }
+                Data.save("settings", this.current);
+            }
+        });
+        this.listeners = new Set();
+        this.defaults = defaults;
+        this.current = { ...defaults, ...Data.load("settings") };
+    }
+    _dispatch() {
+        this._dispatcher.dispatch({ type: "update" });
+    }
+    update(settings) {
+        Object.assign(this.current, typeof settings === "function" ? settings(this.current) : settings);
+        this._dispatch();
+    }
+    reset() {
+        this.current = { ...this.defaults };
+        this._dispatch();
+    }
+    delete(...keys) {
+        for (const key of keys) {
+            delete this.current[key];
+        }
+        this._dispatch();
+    }
+    useCurrent() {
+        return Flux.useStateFromStores([this], () => this.current);
+    }
+    useState() {
+        return Flux.useStateFromStores([this], () => [this.current, (settings) => this.update(settings)]);
+    }
+    useStateWithDefaults() {
+        return Flux.useStateFromStores([this], () => [this.current, this.defaults, (settings) => this.update(settings)]);
+    }
+    useListener(listener) {
+        React.useEffect(() => {
+            this.addListener(listener);
+            return () => this.removeListener(listener);
+        }, [listener]);
+    }
+    addListener(listener) {
+        this.listeners.add(listener);
+        return listener;
+    }
+    removeListener(listener) {
+        this.listeners.delete(listener);
+    }
+    removeAllListeners() {
+        this.listeners.clear();
+    }
+}
+const createSettings = (Data, defaults) => new Settings(Data, defaults);
+
+const createStyles = (id) => {
+    return {
+        inject(styles) {
+            if (typeof styles === "string") {
+                BdApi.DOM.addStyle(id, styles);
+            }
+        },
+        clear: () => BdApi.DOM.removeStyle(id)
+    };
+};
+
+const Button = /* @__PURE__ */ byProps(["Colors", "Link"], { entries: true });
+
+const Flex = /* @__PURE__ */ byProps(["Child", "Justify"], { entries: true });
+
+const { FormSection, FormItem, FormTitle, FormText, FormDivider, FormNotice } = /* @__PURE__ */ demangle({
+    FormSection: bySource$1(".titleClassName", ".sectionTitle"),
+    FormItem: bySource$1(".titleClassName", ".required"),
+    FormTitle: bySource$1(".faded", ".required"),
+    FormText: (target) => target.Types?.INPUT_PLACEHOLDER,
+    FormDivider: bySource$1(".divider", ".style", "\"div\""),
+    FormNotice: bySource$1(".imageData", "formNotice")
+}, ["FormSection", "FormItem", "FormText"]);
+
+const { Link, NavLink, LinkRouter } = /* @__PURE__ */ demangle({
+    NavLink: bySource$1(".sensitive", ".to"),
+    Link: bySource$1(".component"),
+    LinkRouter: bySource$1("this.history")
+}, ["NavLink", "Link"]);
+
+const margins = /* @__PURE__ */ byProps(["marginLarge"]);
+
+const confirm = (title, content, options = {}) => BdApi.UI.showConfirmationModal(title, content, options);
+
+const [getInstanceFromNode, getNodeFromInstance, getFiberCurrentPropsFromNode, enqueueStateRestore, restoreStateIfNeeded, batchedUpdates] = ReactDOM?.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED?.Events ?? [];
+const ReactDOMInternals = {
+    getInstanceFromNode,
+    getNodeFromInstance,
+    getFiberCurrentPropsFromNode,
+    enqueueStateRestore,
+    restoreStateIfNeeded,
+    batchedUpdates
+};
+
+const FCHook = ({ children: { type, props }, callbacks: callbacks }) => {
+    let result = type(props);
+    for (const callback of callbacks) {
+        result = callback(result, props) ?? result;
+    }
+    return result;
+};
+const hookFunctionComponent = (target, callback) => {
+    if (target.type === FCHook) {
+        target.props.callbacks.push(callback);
+    }
+    else {
+        const props = {
+            children: { ...target },
+            callbacks: [callback]
+        };
+        target.props = props;
+        target.type = FCHook;
+    }
+    return target;
+};
+const queryTree = (node, predicate) => {
+    const worklist = [node];
+    while (worklist.length !== 0) {
+        const node = worklist.shift();
+        if (predicate(node)) {
+            return node;
+        }
+        if (node?.props?.children) {
+            worklist.push(...[node.props.children].flat());
+        }
+    }
+    return null;
+};
+const getFiber = (node) => ReactDOMInternals.getInstanceFromNode(node ?? {});
+const queryFiber = (fiber, predicate, direction = "up" , depth = 30, current = 0) => {
+    if (current > depth) {
+        return null;
+    }
+    if (predicate(fiber)) {
+        return fiber;
+    }
+    if ((direction === "up"  || direction === "both" ) && fiber.return) {
+        const result = queryFiber(fiber.return, predicate, "up" , depth, current + 1);
+        if (result) {
+            return result;
+        }
+    }
+    if ((direction === "down"  || direction === "both" ) && fiber.child) {
+        let child = fiber.child;
+        while (child) {
+            const result = queryFiber(child, predicate, "down" , depth, current + 1);
+            if (result) {
+                return result;
+            }
+            child = child.sibling;
+        }
+    }
+    return null;
+};
+const findOwner = (fiber, depth = 50) => {
+    return queryFiber(fiber, (node) => node?.stateNode instanceof React.Component, "up" , depth);
+};
+const forceFullRerender = (fiber) => new Promise((resolve) => {
+    const owner = findOwner(fiber);
+    if (owner) {
+        const { stateNode } = owner;
+        const original = stateNode.render;
+        stateNode.render = function forceRerender() {
+            original.call(this);
+            stateNode.render = original;
+            return null;
+        };
+        stateNode.forceUpdate(() => stateNode.forceUpdate(() => resolve(true)));
+    }
+    else {
+        resolve(false);
+    }
+});
+
+const SettingsContainer = ({ name, children, onReset }) => (React.createElement(FormSection, null,
+    children,
+    React.createElement(FormDivider, { className: classNames(margins.marginTop20, margins.marginBottom20) }),
+    React.createElement(Flex, { justify: Flex.Justify.END },
+        React.createElement(Button, { size: Button.Sizes.SMALL, onClick: () => confirm(name, "Reset all settings?", {
+                onConfirm: () => onReset()
+            }) }, "Reset"))));
+
+const createPlugin = (config, callback) => (meta) => {
+    const name = config.name ?? meta.name;
+    const version = config.version ?? meta.version;
+    const Logger = createLogger(name, "#3a71c1", version);
+    const Lazy = createLazy();
+    const Patcher = createPatcher(name, Logger);
+    const Styles = createStyles(name);
+    const Data = createData(name);
+    const Settings = createSettings(Data, config.settings ?? {});
+    const plugin = callback({ meta, Logger, Lazy, Patcher, Styles, Data, Settings });
+    return {
+        start() {
+            Logger.log("Enabled");
+            Styles.inject(config.styles);
+            plugin.start();
+        },
+        stop() {
+            Lazy.abort();
+            Patcher.unpatchAll();
+            Styles.clear();
+            plugin.stop();
+            Logger.log("Disabled");
+        },
+        getSettingsPanel: plugin.SettingsPanel ? () => (React.createElement(SettingsContainer, { name: name, onReset: () => Settings.reset() },
+            React.createElement(plugin.SettingsPanel, null))) : null
+    };
+};
+
+const styles = ".friendsOnline-2JkivW {\n  color: var(--channels-default);\n  text-align: center;\n  text-transform: uppercase;\n  font-size: 10px;\n  font-weight: 500;\n  line-height: 1.3;\n  width: 70px;\n  word-wrap: normal;\n  white-space: nowrap;\n  cursor: pointer;\n}\n.friendsOnline-2JkivW:hover {\n  color: var(--interactive-hover);\n}";
+
+const GuildsNav = bySource(["guildsnav"], { entries: true });
+const guildStyles = byProps(["guilds", "base"]);
+const treeStyles = byProps(["tree", "scroller"]);
+const listStyles = byProps(["listItem"]);
+const friendsOnline = "friendsOnline-2JkivW";
+const OnlineCount = () => {
+    const online = Flux.useStateFromStores([PresenceStore, RelationshipStore], () => (Object.entries(RelationshipStore.getRelationships())
+        .filter(([id, type]) => type === 1  && PresenceStore.getStatus(id) !== "offline" )
+        .length));
+    return (React.createElement("div", { className: listStyles.listItem },
+        React.createElement(Link, { to: "/channels/@me" },
+            React.createElement("div", { className: friendsOnline },
+                online,
+                " Online"))));
+};
+const index = createPlugin({ styles }, ({ Logger, Patcher }) => {
+    const triggerRerender = async () => {
+        const node = document.getElementsByClassName(guildStyles.guilds)?.[0];
+        const fiber = getFiber(node);
+        if (await forceFullRerender(fiber)) {
+            Logger.log("Rerendered guilds");
+        }
+        else {
+            Logger.warn("Unable to rerender guilds");
+        }
+    };
+    return {
+        start() {
+            Patcher.after(GuildsNav, "type", ({ result }) => {
+                const target = queryTree(result, (node) => node?.props?.className?.split(" ").includes(guildStyles.guilds));
+                if (!target) {
+                    return Logger.error("Unable to find chain patch target");
+                }
+                hookFunctionComponent(target, (result) => {
+                    const scroller = queryTree(result, (node) => node?.props?.className?.split(" ").includes(treeStyles.scroller));
+                    if (!scroller) {
+                        return Logger.error("Unable to find scroller");
+                    }
+                    const { children } = scroller.props;
+                    const homeButtonIndex = children.findIndex((child) => typeof child?.props?.isOnOtherSidebarRoute === "boolean");
+                    children.splice(homeButtonIndex + 1, 0, React.createElement(OnlineCount, null));
+                });
+            });
+            triggerRerender();
+        },
+        stop() {
+            triggerRerender();
+        }
+    };
+});
+
+module.exports = index;
+
+/*@end @*/
