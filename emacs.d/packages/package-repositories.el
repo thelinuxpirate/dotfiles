cd83a2c863ce295dd8a9a93e7b49a3b4387e3997
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2022-11-13 21:56:47 -0800

    Delete emacs.d directory

--- emacs.d/packages/package-repositories.el
+++
@@ -1,6 +0,0 @@
-;; Package Repositries:
-(require 'package)
-(add-to-list 'package-archives '("melpa" .  "https://melpa.org/packages/") t) ;; MELPA Repo gets added;
-(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t) ;; ELPA Repo gets added;
-(package-initialize)
-(package-refresh-contents)
