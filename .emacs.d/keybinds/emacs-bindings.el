efe8d9627777d50526148a4814774f0c4e5c9516
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2022-12-05 18:20:39 -0800

    Delete .emacs.d directory

--- .emacs.d/keybinds/emacs-bindings.el
+++
@@ -1,4 +0,0 @@
-(global-set-key (kbd "C-s") 'eshell)
-(global-set-key (kbd "C-k") 'kill-buffer)
-(global-set-key (kbd "C-l") 'switch-to-buffer)
-(global-set-key (kbd "C-f") 'find-file)
--- .emacs.d/packages/package.el
+++
@@ -1,5 +0,0 @@
-(load-file "/home/thelinuxpirate/.emacs.d/packages/package-repositories.el") ;; Loads Repositories for Packages;
-
-;; EVIL Mode:
-(unless (package-installed-p 'evil)  
-  (package-install 'evil))
