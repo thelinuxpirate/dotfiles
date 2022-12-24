439b2c3f15ca094a8af4e1a1a58ce1a305aae632
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2022-12-23 20:53:58 -0800

    Add files via upload

---
+++ dwm/patch/include.h
@@ -0,0 +1,20 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+
+/* Other patches */
+#include "cyclelayouts.h"
+#include "fullscreen.h"
+/* Layouts */
+#include "layout_bstack.h"
+#include "layout_centeredmaster.h"
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
