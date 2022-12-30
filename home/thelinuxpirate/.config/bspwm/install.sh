465ebed6440497cc2e75b1bf1e6ae207fcf47100
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2022-12-29 21:51:59 -0800

    Fixed incorrect PATH locations
    Changed old "bspwm-config" repo paths to point to "dotfiles" path

--- home/thelinuxpirate/.config/bspwm/install.sh
+++ home/thelinuxpirate/.config/bspwm/install.sh
@@ -1,4 +1,4 @@
-echo "This Script WILL Replace BSPWM Configurations if they are already on your system!"
+echo "This Script WILL Replace ANY BSPWM Configurations if they are already on your system!"
 
 echo "Generating BSPWM Config"
 rm -r ~/.config/bspwm/
@@ -16,14 +16,14 @@ echo "DONE"
 echo "Generating POLYBAR Config (OLD-Piratebar)"
 rm -r ~/.config/polybar/
 mkdir ~/.config/polybar/
-mv ~/bspwm-config/polybar/config.ini ~/.config/polybar/
-mv ~/bspwm-config/polybar/launch.sh ~/.config/polybar/
+mv ~/dotfiles/home/thelinuxpirate/bspwm/polybar/config.ini ~/.config/polybar/
+mv ~/dotfiles/home/thelinuxpirate/bspwm/polybar/launch.sh ~/.config/polybar/
 chmod +x ~/.config/polyabr/launch.sh
 echo "DONE"
 
 echo "Generating PICOM Config"
 rm -r ~/.config/picom/
 mkdir ~/.config/picom/
-mv ~/bspwm-config/picom/picom.conf ~/.config/picom/
+mv ~/dotfiles/home/thelinuxpirate/.config/picom/picom.conf ~/.config/picom/
 chmod +x ~/.config/picom/picom.conf
 echo "DONE"
