71fb2279aab7c18bf66cde8a92d073fb7869fc91
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2023-01-18 20:29:15 -0800

    Updated Build.sh
    Forgot to even install the base files needed for the build ha!

--- home/thelinuxpirate/.config/xmonad/build.sh
+++ home/thelinuxpirate/.config/xmonad/build.sh
@@ -1,12 +1,16 @@
-echo "Generating init file..."
-stack init
+  echo "Cloning XMonad-Base files & XMonad-Contrib files for Installation"
+  git clone https://github.com/xmonad/xmonad && git clone https://github.com/xmonad/xmonad-contrib
+  echo "Getting ready to build!"
+  
+  echo "Generating init file..."
+  stack init
 
-echo "Installing: XMonad + Contrib"
-stack install
-echo "DONE!"
+  echo "Installing: XMonad + Contrib"
+  stack install
+  echo "DONE!"
 
-export PATH=$HOME/.local/bin:$PATH
-echo "Exported ~/.local/bin to your $PATH"
+  export PATH=$HOME/.local/bin:$PATH
+  echo "Exported ~/.local/bin to your $PATH"
 
-echo "Proof of installation:"
-which xmonad
+  echo "Proof of installation:"
+  which xmonad
