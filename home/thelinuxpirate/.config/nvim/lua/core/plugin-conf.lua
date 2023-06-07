40f83f8739063e373f3263e712542d1e88044d75
TLP <spektralfrogadier@gmail.com>
2023-06-07 15:59:19 -0700

    Add files via upload

---
+++ home/thelinuxpirate/.config/nvim/lua/core/plugin-conf.lua
@@ -0,0 +1,26 @@
+-- Startup.nvim
+require("startup").setup({theme = "startify"}) 
+
+-- Telescope
+  local telescope = require('telescope')
+  telescope.setup {
+    pickers = {
+      find_files = {
+        hidden = true
+      }
+    }
+  }
+
+-- Nvim-Tree
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
