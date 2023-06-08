3d8e04731ca99be6e982e436178f74e1e0e42aec
TLP <spektralfrogadier@gmail.com>
2023-06-07 17:33:16 -0700

    Delete home/thelinuxpirate/.stumpwm.d directory

--- home/thelinuxpirate/.stumpwm.d/config/keybinds/bindings.lisp
+++
@@ -1,7 +0,0 @@
-;; -*-lisp-*-
-(set-prefix-key (kbd "s-f"))
-
-(define-key *root-map* (kbd "s-Q") "end-session")
-(define-key *root-map* (kbd "RET") "exec alacritty")
-(define-key *root-map* (kbd "SPC") "exec dmenu_run")
-(define-key *root-map* (kbd "e") "exec emacsclient -c")
