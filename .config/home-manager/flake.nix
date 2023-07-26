f2262ad6566cd6ebb841253a425faf26008eab8a
TLP <spektralfrogadier@gmail.com>
2023-07-26 11:13:36 -0700

    Add files via upload

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Home Manager configuration of pinguino";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { nixpkgs, home-manager, hyprland, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."pinguino" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ 
+        hyprland.homeManagerModules.default
+        {wayland.windowManager.hyprland.enable = true;}
+        ./home.nix 
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
