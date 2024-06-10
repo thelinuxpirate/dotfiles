53eeeac8b27180b42dbb351e770d3ef0559dfc9e
Trong <spektralfrogadier@gmail.com>
2024-06-10 07:57:33 -0700

    desktop update

---
+++ .config/xmonad/lib/Theme/Trong.hs
@@ -0,0 +1,16 @@
+module Theme.Trong where
+
+import XMonad
+
+colorScheme  = "sleepy"
+
+colorBg      = "#101419"
+colorBgAlt   = "#151e2e"
+colorFg      = "#c5dde0"
+colorFgAlt   = "#5d869e"
+
+colorPrimary = "#6eaae6"
+colorAlert   = "#bf4559"
+
+colorBlack   = "#202836"
+colorBlue    = "#6eaae6"
--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,40 +5,45 @@
   https://github.com/Axarva/dotfiles-2.0/
 -}
 
+-- Base
 import XMonad
+import qualified XMonad.StackSet as W
+import Theme.Trong as T
 
 -- Data
+import qualified Data.Map as M
+import Data.Maybe (maybeToList)
 
 -- Hooks
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.InsertPosition (insertPosition, Focus(Newer), Position(End))
+import XMonad.Hooks.ManageHelpers (doFullFloat, isDialog, isFullscreen)
 import XMonad.Hooks.ManageDocks
 
 -- Actions
 import XMonad.Actions.SpawnOn (spawnOn, spawnHere)
+import XMonad.Actions.Submap (submap)
 import XMonad.Actions.MouseResize
-import XMonad.Actions.DwmPromote
 import XMonad.Actions.Minimize
 import XMonad.Actions.CycleWindows
 import XMonad.Actions.GridSelect
 import XMonad.Actions.Search as Se
 
 -- Utilities 
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeysP, additionalKeys)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
--- Layouts
+-- Layouts -- ADD grid; mirror tall;
 import XMonad.Layout.Spacing (smartSpacingWithEdge)
-import XMonad.Layout.Fullscreen
-import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Fullscreen (fullscreenEventHook, fullscreenManageHook, fullscreenSupport, fullscreenFull)
 import XMonad.Layout.Minimize
 import qualified XMonad.Layout.BoringWindows as BW
 
 -- Haskell
 import Control.Monad (join, when)
 import Control.Monad.IO.Class (liftIO)
-import Data.Maybe (maybeToList)
-import System.Process (readProcess)
+import System.Process (readProcessWithExitCode, callProcess)
+import System.Exit (ExitCode(..))
 
 myTerminal :: String
 myTerminal = "wezterm"
@@ -68,26 +73,42 @@ myStartupHook = do
   spawnOnce "nm-applet"
   spawnOn "9" "blueman-applet"
   spawnOnce "emacs --daemon"
-  spawnOnce "feh --bg-fill ~/Pictures/Wallpapers/landscape/paper-city-art.jpg"
-  spawnOnce "polybar -c ~/.config/xmonad/polybar/config"
+  spawnOnce "feh --bg-fill ~/Pictures/Wallpapers/etc/bender.jpg"
+  spawnOnce "polybar -c ~/.config/xmonad/polybar/config.ini"
   spawnOnce "dunst"
   spawnOnce "flameshot"
   spawnOnce "picom --daemon"
+  spawnOnce "greenclip daemon"
   spawnOnce "discover-overlay --configure"
   spawnOnce "playme -t ~/.local/audio/StickerbushSymphony.mp3 -d 1"
 
+myExtraWorkspaces = [(xK_0, "0"),(xK_minus, "gd"),(xK_equal, "tmp")]
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] ++ (map snd myExtraWorkspaces)
 
-myLayouts = avoidStruts (tiled ||| Mirror tiled ||| Full ||| threeCol)
+extraWorkspaces =
+  [
+    ((mod4Mask, key), (windows $ W.greedyView ws))
+    | (key,ws) <- myExtraWorkspaces
+  ] ++ [
+    ((mod4Mask .|. shiftMask, key), (windows $ W.shift ws))
+    | (key,ws) <- myExtraWorkspaces
+  ]
+
+myLayouts = avoidStruts (tiled ||| Mirror tiled ||| Full)
   where
-    threeCol = ThreeColMid nmaster delta ratio
     tiled   = Tall nmaster delta ratio
     nmaster = 1      -- number of windows in the master pane
     ratio   = 2/3    -- proportion of screen occupied by master pane
     delta   = 3/100  -- resize incrementations 
 
 myManageHook :: ManageHook
-myManageHook = composeAll
-  [ insertPosition End Newer ]
+myManageHook = fullscreenManageHook <+> manageDocks <+> composeAll
+  [ insertPosition End Newer
+  , className    =? "Gimp"           --> doFloat
+  , isDialog                         --> doF W.swapUp
+  , resource     =? "desktop_window" --> doIgnore
+  , isFullscreen                     --> doFullFloat
+  ]
 
 addNETSupported :: Atom -> X ()
 addNETSupported x   = withDisplay $ \dpy -> do
@@ -110,12 +131,21 @@ togglePolybar = do
     polybarRunning <- liftIO isPolybarRunning
     if polybarRunning
        then spawn "killall -q polybar"
-       else spawn "polybar example &"
+       else spawnOnce "polybar -c ~/.config/xmonad/polybar/config.ini"
 
 isPolybarRunning :: IO Bool
 isPolybarRunning = do
-    result <- readProcess "pgrep" ["-x", "polybar"] ""
-    return (not $ null result)
+    (exitCode, result, _) <- readProcessWithExitCode "pgrep" ["-x", "polybar"] ""
+    case exitCode of
+        ExitSuccess -> do
+            let running = not $ null result
+            if running
+               then do
+                   let pid = head $ lines result
+                   callProcess "kill" [pid]
+               else return ()
+            return running
+        _ -> return False   
 
 main :: IO ()
 main = xmonad 
@@ -128,7 +158,7 @@ myConfig = def
     { modMask            = mod4Mask
     , startupHook        = myStartupHook >> addEWMHFullscreen
     , terminal           = myTerminal
-    , workspaces         = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    , workspaces         = myWorkspaces
     , layoutHook         = smartSpacingWithEdge 4 $ minimize . BW.boringWindows $ myLayouts
     , manageHook         = myManageHook
     , borderWidth        = myBorderWidth
@@ -141,10 +171,11 @@ myConfig = def
     [ -- Applications 
     ("M-<Return>",    spawn myTerminal)
     , ("M-<Space>",   spawn "rofi -show drun")
+    , ("M-C-c",       spawn "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command \'{cmd}\'")
     , ("M-S-e",       spawn "emacsclient -c")
     , ("M-S-n",       spawn "wezterm -e nvim")
     , ("M-S-b",       spawn "firefox")
-    , ("M-p",         spawnOn "5" "pavucontrol")
+    , ("M-p",         spawnOn "5" $ "pavucontrol")
     , ("M-S-t",       spawn "thunar")
     , ("M-S-s",       spawn "flatpak run com.spotify.Client")
     , ("M-S-d",       spawn "discord")
@@ -153,10 +184,20 @@ myConfig = def
     , ("M-S-r",                  spawn "xmonad --restart")
     , ("M-w",                    kill)
     , ("M-b",                    togglePolybar)
-    , ("M-S-<Return>",           dwmpromote)
+    , ("M-g",                    windows W.focusMaster)
+    , ("M-S-<Return>",           windows W.swapMaster)
+    , ("M-S-<Space>",            withFocused $ windows . W.sink)
     , ("M-m",                    withFocused minimizeWindow)
     , ("M-S-m",                  withLastMinimized maximizeWindowAndFocus)
 
+    -- Layouts Submap
+    , ("M-S-l", submap . M.fromList $
+       [ ((0, xK_t),     sendMessage $ JumpToLayout "Tall")
+       , ((0, xK_f),     sendMessage $ JumpToLayout "Full")
+       , ((0, xK_g),     sendMessage $ JumpToLayout "Grid")
+       --, ((0, xK_space), spawn "mpc toggle")
+       ])
+    
     -- Media Keys
     , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 2%+" )
     , ("<XF86AudioLowerVolume>", spawn "amixer set Master 2%-" )
@@ -168,3 +209,4 @@ myConfig = def
     , ("<XF86Search>",           spawn "rofi -show drun" )
     -- FN Keys (Keyboard Dependant; feel free to modify)
     ]
+    `additionalKeys` (extraWorkspaces)
